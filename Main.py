3ec9193a5984e8bfe7c646cee4f97658934c99fb
pasan_r_k_95 <kaluarachchikapr.24@uom.lk>
2025-11-21 19:40:43 +0530

    first edit

--- Main.py
+++ Main.py
@@ -3,3 +3,8 @@ When detected a movement through the Ultrasonic sensor, a video clip recorded an
 Then starts analyzing it and gets count of people went in and out of the classroom.
 '''
 
+from time import time
+import cv2
+from picamera2 import Picamera2
+from gpiozero import LED, UltrasonicSensor
+
